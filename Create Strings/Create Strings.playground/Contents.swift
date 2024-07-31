b837bddbdf9b6d1f62f3a14f253c06e63d3ba0df
Phantomwatchdog <8706@holbertonstudents.com>
2024-07-31 15:09:20 +0200

    Test strings use

--- Create Strings/Create Strings.playground/Contents.swift
+++ Create Strings/Create Strings.playground/Contents.swift
@@ -1,3 +1,23 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+let actor = "Liam Neeson"
+let filename = "Day.jpg"
+let result = "⭐️ You win! ⭐️"
+
+let quote = "Then he tapped a sign saying \"Believe\" and walked away"
+
+let movie = """
+a multilines
+test for a
+string
+"""
+
+let nameLength = actor.count
+print(nameLength) // 11
+
+print(actor.uppercased()) // LIAM NEESON
+
+print(movie.hasPrefix("a multilines")) // True
+print(movie.hasSuffix("string")) // True
+print(movie.hasSuffix("String")) // False
+print(result.hasPrefix("⭐️")) // True
