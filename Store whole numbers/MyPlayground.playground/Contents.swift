23367f57942067291ed0a8af4a1df76f574fcf8a
Phantomwatchdog <8706@holbertonstudents.com>
2024-07-31 15:40:21 +0200

    Testing storage numbers

---
+++ Store whole numbers/MyPlayground.playground/Contents.swift
@@ -0,0 +1,19 @@
+import Cocoa
+
+let score = 10
+let reallyBig = 1_000_000_000
+
+let minusFive = score - 5
+let plusFive = score + 5
+let Double = score * 2
+let Square = score * score
+let DividedByTwo = score / 2
+
+var count = 20
+count = count + 10
+count += 20
+print(count)
+
+let TestNumber = 180
+print(TestNumber.isMultiple(of: 3)) // True
+print(25.isMultiple(of: 5)) // True
