362cf4463ba2dafcf155657a72d65b275c26a4a5
Phantomwatchdog <8706@holbertonstudents.com>
2024-07-31 13:53:56 +0200

    Test push Swift code

---
+++ Contents.swift
@@ -0,0 +1,15 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+var name = "Ted"
+name = "Rebecca"
+name = "Keeley"
+
+let character = "Daphne"
+
+var playname = "Roy"
+print(playname)
+
+playname = "John"
+print(playname)
