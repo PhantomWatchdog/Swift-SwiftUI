c5fbebff023bd11d57e8c5a05d82ca52fc3a430d
Phantomwatchdog <8706@holbertonstudents.com>
2024-07-31 14:13:14 +0200

    Create folder for test var and const

---
+++ Create variables and constants/Contents.swift
@@ -0,0 +1,19 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+var name = "Ted"
+name = "Rebecca"
+name = "Keeley"
+print(name)
+
+let character = "Daphne"
+
+var playname = "Roy"
+print(playname)
+
+playname = "John"
+print(playname)
+
+playname = "Lucy"
+print(playname)
