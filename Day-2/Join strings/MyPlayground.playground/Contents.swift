41a91c6e5b560e2b3bf1ebaa8da7904a4209ff63
Phantomwatchdog <8706@holbertonstudents.com>
2024-08-01 23:29:14 +0200

    Tested  different use of string

---
+++ Day-2/Join strings/MyPlayground.playground/Contents.swift
@@ -0,0 +1,24 @@
+import Cocoa
+
+// concatanate strings
+let people = "Haters"
+let action = "hate"
+let lyric = people + " gonna " + action
+print(lyric)
+
+let numCode = "1" + "2" + "3" + "4" + "5"
+print(numCode)
+
+// use of constants in string
+let name = "Blackkeiser"
+let age = 28
+let message = "Hello, here is \(name) ! Do you want to play a game for \(age) years ?"
+print(message)
+
+// Use of Int type constant in string
+let number = 13
+let mayday = "Apollo " + String(number) + " is going crash !"
+print(mayday)
+
+// Calculation
+print("15 * 10 = \(15 * 10)")
