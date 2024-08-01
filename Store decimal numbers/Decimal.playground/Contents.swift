8ea0c1c1560b6115c5d497e128403eb47cdf44ec
Phantomwatchdog <8706@holbertonstudents.com>
2024-08-01 11:29:41 +0200

    Test storage of integer and decimal

---
+++ Store decimal numbers/Decimal.playground/Contents.swift
@@ -0,0 +1,22 @@
+import Cocoa
+
+let number = 0.1 + 0.2
+print(number)
+
+let a = 1
+let b = 3.0
+let c = a + Int(b)
+print(c)
+
+let d = 1
+let e = 3.0
+let f = Double(a) + b
+print(f)
+
+let double1 = 3.7
+let double2 = 50.50
+let int1 = 10
+let g = double1 + double2 + Double(int1)
+let h = Int(double1) + Int(double2) + int1
+print(g)
+print(h)
