190daf3a432fd26739c5f177f61bbc0f24dc9d04
Phantomwatchdog <8706@holbertonstudents.com>
2024-08-01 23:01:06 +0200

    Boolean storage

---
+++ Day-2/Store with Boolean/MyPlayground.playground/Contents.swift
@@ -0,0 +1,15 @@
+import Cocoa
+
+let filename = "paris.jpg"
+print(filename.hasSuffix(".jpg"))
+
+let number = 120
+print(number.isMultiple(of: 3))
+
+var goodboy = true
+let looser = false
+goodboy.toggle()
+print(goodboy)
+
+var isAuthenticated = false
+isAuthenticated = !isAuthenticated
